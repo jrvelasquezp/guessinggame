64cff298f1b161776d683f58b4aa8d1c450b41ec
jrvelasquezp <jrvelasquezp@gmail.com>
2020-04-09 17:12:08 -0500

    Verify a critical error that prevents execution.

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,30 +2,27 @@
 #file guessinggame.sh
 #Created by Richard Velasquez. 2020
 
-#This file should contain one function
+#It should contain at least one function
 function number_files_dir {
  local numf=0
  let numf=$(ls -1 $PWD | wc -l)
  echo $numf
 }
-#initialize guess_num . Negative value ensures proper initialize.
-guess_num=-1 
-
+#Initialize guess_num. Negative values ensure proper initialize
+guess_num=-1
 #One loop
-while [[ $(number_files_dir) -ne $guess_num]]
+while [[ $(number_files_dir) -ne $guess_num ]]
 do
- echo "Type how many files should have this directory and press Enter"
+ echo "Type how many files should have this directory and press Enter:"
  read guess_num
-#one if statement
+#one if
  if [[ $(number_files_dir) -gt $guess_num ]]
  then
-  echo "This number is too small. Try again."
+  echo "This number is too big. Try again."
  elif [[ $(number_files_dir) -lt $guess_num ]]
  then
-  echo "This number is too big. Try again."
+  echo "This number is too small. Try again."
  else
-  echo "Congratulations. This directory contains $guess_num files inside."
+  echo "Congratulations, this directory has $guess_num files inside."
  fi
 done
-
-
