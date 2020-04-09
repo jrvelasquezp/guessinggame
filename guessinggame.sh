2a1101190767f0789adde8e04fcb95c72c69a8a4
jrvelasquezp <jrvelasquezp@gmail.com>
2020-04-09 18:00:08 -0500

    Check if entry is a valid number added. It will work in directory with spaces inside name, and count hidden files

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,7 +5,7 @@
 #It should contain at least one function
 function number_files_dir {
  local numf=0
- let numf=$(ls -1 $PWD | wc -l)
+ let numf=$(ls -1A "$PWD" | wc -l)
  echo $numf
 }
 #Initialize guess_num. Negative values ensure proper initialize
@@ -15,14 +15,21 @@ while [[ $(number_files_dir) -ne $guess_num ]]
 do
  echo "Type how many files should have this directory and press Enter:"
  read guess_num
-#one if
+#if statement to detect if entry is not a number
+if [[ $guess_num =~ ^[0-9]+$ ]]
+then
+#another if
  if [[ $(number_files_dir) -gt $guess_num ]]
  then
-  echo "This number is too big. Try again."
+  echo "This number is too small. Try again."
  elif [[ $(number_files_dir) -lt $guess_num ]]
  then
-  echo "This number is too small. Try again."
+  echo "This number is too big. Try again."
  else
   echo "Congratulations, this directory has $guess_num files inside."
  fi
+ else
+  echo "This is not a number. Try again."
+  guess_num=-1	
+fi
 done
