e5174483e0fb0b554f04c689438bc890fde89d70
jrvelasquezp <jrvelasquezp@gmail.com>
2020-04-09 15:07:27 -0500

    Add while loop, modify else condition and add congratulations message on guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,16 +8,24 @@ function number_files_dir {
  let numf=$(ls -1 $PWD | wc -l)
  echo $numf
 }
+#initialize guess_num . Negative value ensures proper initialize.
+guess_num=-1 
 
-echo "Type how many files should have this directory and press Enter"
-read guess_num
-#one if statement
-if [[ $(number_files_dir) -gt $guess_num ]]
-then
- echo "This number is too small. Try again."
-elif [[ $(number_files_dir) -lt $guess_num ]]
-then
- echo "This number is too big. Try again."
-fi
 #One loop
+while [[ $(number_files_dir) -ne $guess_num]]
+do
+ echo "Type how many files should have this directory and press Enter"
+ read guess_num
+#one if statement
+ if [[ $(number_files_dir) -gt $guess_num ]]
+ then
+  echo "This number is too small. Try again."
+ elif [[ $(number_files_dir) -lt $guess_num ]]
+ then
+  echo "This number is too big. Try again."
+ else
+  echo "Congratulations. This directory contains $guess_num files inside."
+ fi
+done
+
 
